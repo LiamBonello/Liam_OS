c3ed044b5b126a636e3a0331146e8f7ff88598ae
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 22:08:45 +0200

    Route x86_64 critical exceptions through IST gates

--- core/kernel/arch/x86_64/idt.h
+++ core/kernel/arch/x86_64/idt.h
@@ -3,17 +3,30 @@
 
 #include "types.h"
 
+#define X86_64_IDT_NMI_VECTOR 2U
+#define X86_64_IDT_NMI_IST 2U
 #define X86_64_IDT_DOUBLE_FAULT_VECTOR 8U
 #define X86_64_IDT_DOUBLE_FAULT_IST 1U
+#define X86_64_IDT_PAGE_FAULT_VECTOR 14U
+#define X86_64_IDT_PAGE_FAULT_IST 3U
 
 struct x86_64_idt_state {
     u64 idtr_base;
     u16 idtr_limit;
     u32 exception_gates;
+    u32 nmi_vector;
+    u32 nmi_ist;
+    u32 nmi_present;
+    u32 nmi_ist_ok;
     u32 double_fault_vector;
     u32 double_fault_ist;
     u32 double_fault_present;
     u32 double_fault_ist_ok;
+    u32 page_fault_vector;
+    u32 page_fault_ist;
+    u32 page_fault_present;
+    u32 page_fault_ist_ok;
+    u32 ist_gates_ok;
 };
 
 void x86_64_idt_init(void);
