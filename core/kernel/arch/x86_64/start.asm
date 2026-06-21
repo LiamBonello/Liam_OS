5553d0a96cd5e0a5667b75bcc9222b656ac6ce35
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 19:47:52 +0200

    Call x86_64 C kernel entry from long mode

--- core/kernel/arch/x86_64/start.asm
+++ core/kernel/arch/x86_64/start.asm
@@ -1,26 +1,13 @@
 global x86_64_start
+extern kernel_main_x86_64
 
 section .text
 bits 64
 
 x86_64_start:
-    mov rdi, 0xB8000
-    mov rsi, boot_message
-    mov ah, 0x0F
-
-.write_loop:
-    lodsb
-    test al, al
-    jz .hang
-    mov [rdi], ax
-    add rdi, 2
-    jmp .write_loop
+    call kernel_main_x86_64
 
 .hang:
     cli
     hlt
     jmp .hang
-
-section .rodata
-boot_message:
-    db 'Liam_OS x86_64 long mode online', 0
