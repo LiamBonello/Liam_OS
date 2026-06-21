766278932cf09a7615546adb18f5cb35e052a688
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 20:41:27 +0200

    Retain x86_64 Multiboot2 memory regions

--- core/kernel/arch/x86_64/boot_info.h
+++ core/kernel/arch/x86_64/boot_info.h
@@ -4,6 +4,14 @@
 #include "types.h"
 
 #define X86_64_BOOTLOADER_NAME_MAX 64U
+#define X86_64_MEMORY_REGION_MAX 32U
+#define X86_64_MEMORY_REGION_AVAILABLE 1U
+
+struct x86_64_memory_region {
+    u64 base;
+    u64 length;
+    u32 type;
+};
 
 struct x86_64_boot_summary {
     u32 magic;
@@ -17,7 +25,9 @@ struct x86_64_boot_summary {
     u32 mem_upper_kib;
     u32 mmap_found;
     u32 mmap_entry_count;
+    u32 memory_region_count;
     u64 usable_memory_bytes;
+    struct x86_64_memory_region memory_regions[X86_64_MEMORY_REGION_MAX];
 };
 
 void x86_64_boot_info_parse(u32 magic, u32 boot_info_addr, struct x86_64_boot_summary *summary);
