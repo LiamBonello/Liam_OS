f0a8475ab3a22eb44a53f575d7407ac0feca9792
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 20:42:55 +0200

    Initialize x86_64 PMM plan in boot context

--- core/kernel/arch/x86_64/boot_context.c
+++ core/kernel/arch/x86_64/boot_context.c
@@ -4,4 +4,5 @@ void x86_64_boot_context_init(u32 magic, u32 boot_info_addr, struct x86_64_boot_
 {
     x86_64_boot_info_parse(magic, boot_info_addr, &context->boot_info);
     x86_64_memory_layout_init(&context->memory_layout);
+    x86_64_pmm_plan_init(&context->boot_info, &context->memory_layout, &context->pmm_plan);
 }
