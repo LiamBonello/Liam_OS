ab3ad64387175621efbce174ab3295a66bd02116
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 21:55:02 +0200

    Route x86_64 double fault through IST1

--- core/kernel/arch/x86_64/idt.h
+++ core/kernel/arch/x86_64/idt.h
@@ -3,7 +3,21 @@
 
 #include "types.h"
 
+#define X86_64_IDT_DOUBLE_FAULT_VECTOR 8U
+#define X86_64_IDT_DOUBLE_FAULT_IST 1U
+
+struct x86_64_idt_state {
+    u64 idtr_base;
+    u16 idtr_limit;
+    u32 exception_gates;
+    u32 double_fault_vector;
+    u32 double_fault_ist;
+    u32 double_fault_present;
+    u32 double_fault_ist_ok;
+};
+
 void x86_64_idt_init(void);
+void x86_64_idt_get_state(struct x86_64_idt_state *state);
 void x86_64_exception_handler(u64 vector, u64 error_code);
 
 #endif
