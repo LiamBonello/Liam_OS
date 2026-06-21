20d1b8c474cec6f5bf6263b2c8768929c5362809
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 20:08:29 +0200

    Extend x86_64 early console formatting helpers

--- core/kernel/arch/x86_64/console.h
+++ core/kernel/arch/x86_64/console.h
@@ -5,11 +5,15 @@
 
 void x86_64_console_init(void);
 void x86_64_console_write_at(const char *message, usize row, usize column);
+void x86_64_console_write_u32(usize row, usize column, const char *label, u32 value);
 void x86_64_console_write_hex32(usize row, usize column, const char *label, u32 value);
+void x86_64_console_write_hex64(usize row, usize column, const char *label, u64 value);
 
 void x86_64_serial_init(void);
 void x86_64_serial_write(const char *message);
 void x86_64_serial_write_line(const char *message);
+void x86_64_serial_write_u32(const char *label, u32 value);
 void x86_64_serial_write_hex32(const char *label, u32 value);
+void x86_64_serial_write_hex64(const char *label, u64 value);
 
 #endif
