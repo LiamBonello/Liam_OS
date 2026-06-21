1ba6d46271900807b30cae5ffe7a454f3a593cb5
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 21:31:51 +0200

    Add x86_64 bootstrap TSS planning state

---
+++ core/kernel/arch/x86_64/tss.h
@@ -0,0 +1,24 @@
+#ifndef LIAM_OS_X86_64_TSS_H
+#define LIAM_OS_X86_64_TSS_H
+
+#include "types.h"
+
+#define X86_64_TSS_IST_STACK_BYTES 4096ULL
+#define X86_64_TSS_IST_COUNT 3U
+#define X86_64_TSS_EXPECTED_LIMIT 103U
+
+struct x86_64_tss_state {
+    u64 tss_base;
+    u32 tss_limit;
+    u64 rsp0;
+    u64 ist1;
+    u64 ist2;
+    u64 ist3;
+    u64 ist_stack_bytes;
+    u32 initialized;
+    u32 loaded;
+};
+
+void x86_64_tss_init(struct x86_64_tss_state *state);
+
+#endif
