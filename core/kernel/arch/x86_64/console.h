d3abe4234017ca90205bff8f15d8d102b41a56e0
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-21 19:55:20 +0200

    Add x86_64 early console interface

---
+++ core/kernel/arch/x86_64/console.h
@@ -0,0 +1,15 @@
+#ifndef LIAM_OS_X86_64_CONSOLE_H
+#define LIAM_OS_X86_64_CONSOLE_H
+
+#include "types.h"
+
+void x86_64_console_init(void);
+void x86_64_console_write_at(const char *message, usize row, usize column);
+void x86_64_console_write_hex32(usize row, usize column, const char *label, u32 value);
+
+void x86_64_serial_init(void);
+void x86_64_serial_write(const char *message);
+void x86_64_serial_write_line(const char *message);
+void x86_64_serial_write_hex32(const char *label, u32 value);
+
+#endif
