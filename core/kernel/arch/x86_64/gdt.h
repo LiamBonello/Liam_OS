6b82b006dba44e2aa10ae35078f7f50b9fb36d55
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-22 17:22:10 +0200

    x86_64: add user-mode GDT selectors

--- core/kernel/arch/x86_64/gdt.h
+++ core/kernel/arch/x86_64/gdt.h
@@ -3,10 +3,13 @@
 
 #include "types.h"
 
-#define X86_64_GDT_ENTRY_COUNT 5U
+#define X86_64_GDT_ENTRY_COUNT 7U
 #define X86_64_GDT_CODE_SELECTOR 0x08U
 #define X86_64_GDT_DATA_SELECTOR 0x10U
 #define X86_64_GDT_TSS_SELECTOR 0x18U
+#define X86_64_GDT_USER_DATA_SELECTOR 0x2BU
+#define X86_64_GDT_USER_CODE_SELECTOR 0x33U
+#define X86_64_GDT_SYSCALL_USER_SELECTOR_BASE 0x20U
 #define X86_64_GDT_EXPECTED_LIMIT ((X86_64_GDT_ENTRY_COUNT * 8U) - 1U)
 
 struct x86_64_tss_state;
@@ -17,6 +20,9 @@ struct x86_64_gdt_state {
     u16 code_selector;
     u16 data_selector;
     u16 tss_selector;
+    u16 user_data_selector;
+    u16 user_code_selector;
+    u16 syscall_user_selector_base;
     u16 current_cs;
     u16 current_ds;
     u16 current_ss;
@@ -26,9 +32,12 @@ struct x86_64_gdt_state {
     u64 data_descriptor;
     u64 tss_descriptor_low;
     u64 tss_descriptor_high;
+    u64 user_data_descriptor;
+    u64 user_code_descriptor;
     u32 entry_count;
     u32 limit_ok;
     u32 selectors_ok;
+    u32 user_selectors_ok;
     u32 tss_loaded;
 };
 
