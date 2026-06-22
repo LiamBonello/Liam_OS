d27aa2ba4b3c05219287b93a2412a67891fdd6aa
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-22 17:33:29 +0200

    Wire x86_64 process smoke to userland foundation state

--- core/kernel/arch/x86_64/process.h
+++ core/kernel/arch/x86_64/process.h
@@ -45,6 +45,7 @@ struct x86_64_process_smoke_state {
     u32 last_run_pid;
     u32 worker_a_count;
     u32 worker_b_count;
+    u32 userland_foundation_ok;
     u32 smoke_ok;
     u64 first_stack_base;
     u64 first_stack_top;
