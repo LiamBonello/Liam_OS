a94d2232d74fbfb6861219869737bf869c23cac0
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-22 08:35:05 +0200

    Bump Core version to 0.8.22-dev

--- core/kernel/core/version.h
+++ core/kernel/core/version.h
@@ -2,7 +2,7 @@
 #define LIAM_OS_VERSION_H
 
 #define LIAM_OS_NAME "Liam OS"
-#define LIAM_OS_VERSION "0.8.21-dev"
+#define LIAM_OS_VERSION "0.8.22-dev"
 #define LIAM_OS_CODENAME "Genesis"
 #define LIAM_OS_ARCH "i386"
 #define LIAM_OS_BOOT_PROTOCOL "Multiboot"
