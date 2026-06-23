037aba01a98aba238b607c619b59e429e31ea3f2
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 14:11:34 +0200

    Exercise x86_64 read syscall from user init

--- core/userland/x86_64/init/init.asm
+++ core/userland/x86_64/init/init.asm
@@ -2,9 +2,11 @@ bits 64
 
 %define LIAM_SYSCALL_EXIT 1
 %define LIAM_SYSCALL_WRITE 2
+%define LIAM_SYSCALL_READ 4
 %define LIAM_SYSCALL_GETPID 9
 %define LIAM_SYSCALL_YIELD 10
 
+%define LIAM_STDIN 0
 %define LIAM_STDOUT 1
 
 section .text
@@ -18,6 +20,12 @@ _start:
     mov edi, LIAM_STDOUT
     lea rsi, [rel shell_banner]
     mov edx, shell_banner_len
+    syscall
+
+    mov eax, LIAM_SYSCALL_READ
+    mov edi, LIAM_STDIN
+    lea rsi, [rel input_buffer]
+    mov edx, input_buffer_len
     syscall
 
     mov eax, LIAM_SYSCALL_YIELD
@@ -36,3 +44,9 @@ shell_banner:
     db "Liam_OS x86_64 shell online", 10
     db "$ "
 shell_banner_len equ $ - shell_banner
+
+section .bss
+align 16
+input_buffer:
+    resb 64
+input_buffer_len equ 64
