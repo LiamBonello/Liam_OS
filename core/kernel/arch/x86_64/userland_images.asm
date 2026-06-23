e4ca35f38fc556b02c8a97edf15b744d3369f732
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 12:22:59 +0200

    Embed x86_64 init ELF image

--- core/kernel/arch/x86_64/userland_images.asm
+++ core/kernel/arch/x86_64/userland_images.asm
@@ -4,5 +4,5 @@ global x86_64_user_init_image_end
 section .rodata
 align 16
 x86_64_user_init_image_start:
-    incbin "build/x86_64/userland/init.bin"
+    incbin "build/x86_64/userland/init.elf"
 x86_64_user_init_image_end:
