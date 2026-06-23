0068ee0b7205022c6d7339a37bec7ff57f2f9ce8
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 10:23:45 +0200

    Track live x86_64 user syscall dispatch state

--- core/kernel/arch/x86_64/user_mode.h
+++ core/kernel/arch/x86_64/user_mode.h
@@ -2,6 +2,7 @@
 #define LIAM_OS_X86_64_USER_MODE_H
 
 #include "paging_builder.h"
+#include "syscall_dispatch.h"
 #include "types.h"
 
 #define X86_64_USER_MODE_SYSCALL_EXIT_SENTINEL 0xFFFFFFFFFFFFFFFFULL
@@ -16,6 +17,9 @@ struct x86_64_user_mode_smoke_state {
     u32 yield_ok;
     u32 exit_ok;
     u32 unexpected_syscall;
+    u32 live_dispatcher_initialized;
+    u32 live_dispatcher_ok;
+    u32 syscall_frame_ok;
     u32 user_entry_ready;
     u32 user_stack_ready;
     u32 user_mode_ok;
@@ -25,12 +29,14 @@ struct x86_64_user_mode_smoke_state {
     u64 entry_rsp;
     u64 last_syscall;
     u64 last_result;
+    u64 last_user_rip;
+    u64 last_user_rflags;
 };
 
 extern u64 x86_64_user_smoke_kernel_rsp;
 
 void x86_64_user_smoke_enter(u64 entry_rip, u64 entry_rsp);
-u64 x86_64_user_smoke_syscall(u64 syscall_number, u64 arg0);
+u64 x86_64_user_mode_syscall_entry(struct x86_64_syscall_frame *frame);
 void x86_64_user_mode_run_smoke(struct x86_64_user_mode_smoke_state *state,
                                 const struct x86_64_paging_builder_state *paging_builder,
                                 u32 current_pid);
