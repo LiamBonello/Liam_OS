cabc3421fb930ec5919fb6f6c45db2777afe1c20
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 09:06:32 +0200

    Track x86_64 user context smoke in process state

--- core/kernel/arch/x86_64/process.h
+++ core/kernel/arch/x86_64/process.h
@@ -47,6 +47,7 @@ struct x86_64_process_smoke_state {
     u32 worker_b_count;
     u32 userland_foundation_ok;
     u32 syscall_dispatcher_ok;
+    u32 user_context_ok;
     u32 smoke_ok;
     u64 first_stack_base;
     u64 first_stack_top;
