e961232c29c1ac21ad257f91197f2c4399a59c67
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 11:39:36 +0200

    Add x86_64 user init smoke source

---
+++ core/userland/x86_64/init/init.asm
@@ -0,0 +1,22 @@
+bits 64
+
+%define LIAM_SYSCALL_EXIT 1
+%define LIAM_SYSCALL_GETPID 9
+%define LIAM_SYSCALL_YIELD 10
+
+global _start
+
+_start:
+    mov eax, LIAM_SYSCALL_GETPID
+    syscall
+
+    mov eax, LIAM_SYSCALL_YIELD
+    syscall
+
+    mov eax, LIAM_SYSCALL_EXIT
+    xor edi, edi
+    syscall
+
+.halt:
+    hlt
+    jmp .halt
