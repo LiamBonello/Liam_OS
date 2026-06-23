f3a1626dd4bd5222c6c9b94c1dcafc2681da11df
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 13:27:17 +0200

    Add x86_64 user shell banner

--- core/userland/x86_64/init/init.asm
+++ core/userland/x86_64/init/init.asm
@@ -1,6 +1,7 @@
 bits 64
 
 %define LIAM_SYSCALL_EXIT 1
+%define LIAM_SYSCALL_WRITE 2
 %define LIAM_SYSCALL_GETPID 9
 %define LIAM_SYSCALL_YIELD 10
 
@@ -9,6 +10,11 @@ global _start
 
 _start:
     mov eax, LIAM_SYSCALL_GETPID
+    syscall
+
+    mov eax, LIAM_SYSCALL_WRITE
+    lea rdi, [rel shell_banner]
+    mov esi, shell_banner_len
     syscall
 
     mov eax, LIAM_SYSCALL_YIELD
@@ -21,3 +27,9 @@ _start:
 .halt:
     hlt
     jmp .halt
+
+section .rodata
+shell_banner:
+    db "Liam_OS x86_64 shell online", 10
+    db "$ "
+shell_banner_len equ $ - shell_banner
