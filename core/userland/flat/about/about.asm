f35c7aba080290386085503a50ef747dd604d635
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 12:23:31 +0200

    Update about version to 0.8.60

--- core/userland/flat/about/about.asm
+++ core/userland/flat/about/about.asm
@@ -23,7 +23,7 @@ _start:
 
 .about_message:
     db 'Liam_OS Core', 10
-    db 'Version: 0.8.59-dev', 10
+    db 'Version: 0.8.60-dev', 10
     db 'Architecture: i386', 10
     db 'Userspace: flat binaries, ELF32, and userland-built binaries', 10
 .about_message_len equ $ - .about_message
