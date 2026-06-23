da32357978c629dec484e689e5bff40108ad3915
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-23 11:43:34 +0200

    Bump version for x86_64 embedded user image

--- core/kernel/core/version.h
+++ core/kernel/core/version.h
@@ -2,7 +2,7 @@
 #define LIAM_OS_VERSION_H
 
 #define LIAM_OS_NAME "Liam OS"
-#define LIAM_OS_VERSION "0.8.58-dev"
+#define LIAM_OS_VERSION "0.8.59-dev"
 #define LIAM_OS_CODENAME "Genesis"
 #define LIAM_OS_ARCH "i386"
 #define LIAM_OS_BOOT_PROTOCOL "Multiboot"
