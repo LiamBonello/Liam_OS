db8b37516e66ccb1a1a5b9525c4de38eeb458b3a
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-24 14:16:06 +0200

    Reset user stack on successful exec

--- core/kernel/arch/x86_64/idt_stubs.asm
+++ core/kernel/arch/x86_64/idt_stubs.asm
@@ -1,35 +1,169 @@
-global x86_64_isr_table
-global x86_64_irq_table
-global x86_64_syscall_entry_stub
-extern x86_64_exception_handler
-extern x86_64_irq_handler
-extern x86_64_user_smoke_kernel_rsp
-extern x86_64_user_mode_syscall_entry
-
-section .text
 bits 64
 
+global x86_64_idt_load
+global x86_64_isr_stub_table
+global x86_64_irq_stub_table
+global x86_64_syscall_entry_stub
+
+extern x86_64_isr_dispatch
+extern x86_64_irq_dispatch
+extern x86_64_user_mode_exit_to_kernel
+extern x86_64_user_mode_syscall_entry
+
 %macro ISR_NOERR 1
-global x86_64_isr%1
-x86_64_isr%1:
-    push 0
-    push %1
+global x86_64_isr_stub_%1
+x86_64_isr_stub_%1:
+    push qword 0
+    push qword %1
     jmp x86_64_isr_common
 %endmacro
 
 %macro ISR_ERR 1
-global x86_64_isr%1
-x86_64_isr%1:
-    push %1
+global x86_64_isr_stub_%1
+x86_64_isr_stub_%1:
+    push qword %1
     jmp x86_64_isr_common
 %endmacro
 
-%macro IRQ_STUB 2
-global x86_64_irq%1
-x86_64_irq%1:
-    push %2
+x86_64_idt_load:
+    lidt [rdi]
+    ret
+
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
+ISR_NOERR 4
+ISR_NOERR 5
+ISR_NOERR 6
+ISR_NOERR 7
+ISR_ERR 8
+ISR_NOERR 9
+ISR_ERR 10
+ISR_ERR 11
+ISR_ERR 12
+ISR_ERR 13
+ISR_ERR 14
+ISR_NOERR 15
+ISR_NOERR 16
+ISR_ERR 17
+ISR_NOERR 18
+ISR_NOERR 19
+ISR_NOERR 20
+ISR_NOERR 21
+ISR_NOERR 22
+ISR_NOERR 23
+ISR_NOERR 24
+ISR_NOERR 25
+ISR_NOERR 26
+ISR_NOERR 27
+ISR_NOERR 28
+ISR_ERR 29
+ISR_ERR 30
+ISR_NOERR 31
+
+x86_64_isr_common:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov rdi, rsp
+    call x86_64_isr_dispatch
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+
+    add rsp, 16
+    iretq
+
+%macro IRQ_STUB 1
+global x86_64_irq_stub_%1
+x86_64_irq_stub_%1:
+    push qword 0
+    push qword (32 + %1)
     jmp x86_64_irq_common
 %endmacro
+
+IRQ_STUB 0
+IRQ_STUB 1
+IRQ_STUB 2
+IRQ_STUB 3
+IRQ_STUB 4
+IRQ_STUB 5
+IRQ_STUB 6
+IRQ_STUB 7
+IRQ_STUB 8
+IRQ_STUB 9
+IRQ_STUB 10
+IRQ_STUB 11
+IRQ_STUB 12
+IRQ_STUB 13
+IRQ_STUB 14
+IRQ_STUB 15
+
+x86_64_irq_common:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov rdi, rsp
+    call x86_64_irq_dispatch
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+
+    add rsp, 16
+    iretq
 
 x86_64_syscall_entry_stub:
     push rcx
@@ -56,6 +190,14 @@ x86_64_syscall_entry_stub:
     cmp dword [rsp + 80], 1
     je .exit_to_kernel
 
+    xor r10d, r10d
+    cmp qword [rsp + 0], 8
+    jne .restore_user_return
+    cmp qword [rsp + 72], 0
+    jne .restore_user_return
+    mov r10d, 1
+
+.restore_user_return:
     mov rcx, [rsp + 56]
     mov [rsp + 104], rcx
     mov r11, [rsp + 64]
@@ -64,152 +206,30 @@ x86_64_syscall_entry_stub:
     add rsp, 96
     pop r11
     pop rcx
+    cmp r10d, 1
+    jne .sysret_to_user
+    mov rsp, 0x000000007FFFFFF0
+.sysret_to_user:
     o64 sysret
 
 .exit_to_kernel:
-    mov rsp, [rel x86_64_user_smoke_kernel_rsp]
-    ret
-
-x86_64_isr_common:
-    mov rdi, [rsp]
-    mov rsi, [rsp + 8]
-    and rsp, -16
-    call x86_64_exception_handler
-
-.hang:
-    cli
-    hlt
-    jmp .hang
-
-x86_64_irq_common:
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push r8
-    push r9
-    push r10
-    push r11
-
-    mov rdi, [rsp + 80]
-    call x86_64_irq_handler
-
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-    add rsp, 8
-    iretq
-
-ISR_NOERR 0
-ISR_NOERR 1
-ISR_NOERR 2
-ISR_NOERR 3
-ISR_NOERR 4
-ISR_NOERR 5
-ISR_NOERR 6
-ISR_NOERR 7
-ISR_ERR 8
-ISR_NOERR 9
-ISR_ERR 10
-ISR_ERR 11
-ISR_ERR 12
-ISR_ERR 13
-ISR_ERR 14
-ISR_NOERR 15
-ISR_NOERR 16
-ISR_ERR 17
-ISR_NOERR 18
-ISR_NOERR 19
-ISR_NOERR 20
-ISR_ERR 21
-ISR_NOERR 22
-ISR_NOERR 23
-ISR_NOERR 24
-ISR_NOERR 25
-ISR_NOERR 26
-ISR_NOERR 27
-ISR_NOERR 28
-ISR_ERR 29
-ISR_ERR 30
-ISR_NOERR 31
-
-IRQ_STUB 0, 32
-IRQ_STUB 1, 33
-IRQ_STUB 2, 34
-IRQ_STUB 3, 35
-IRQ_STUB 4, 36
-IRQ_STUB 5, 37
-IRQ_STUB 6, 38
-IRQ_STUB 7, 39
-IRQ_STUB 8, 40
-IRQ_STUB 9, 41
-IRQ_STUB 10, 42
-IRQ_STUB 11, 43
-IRQ_STUB 12, 44
-IRQ_STUB 13, 45
-IRQ_STUB 14, 46
-IRQ_STUB 15, 47
+    add rsp, 96
+    add rsp, 16
+    jmp x86_64_user_mode_exit_to_kernel
 
 section .rodata
 align 8
-x86_64_isr_table:
-    dq x86_64_isr0
-    dq x86_64_isr1
-    dq x86_64_isr2
-    dq x86_64_isr3
-    dq x86_64_isr4
-    dq x86_64_isr5
-    dq x86_64_isr6
-    dq x86_64_isr7
-    dq x86_64_isr8
-    dq x86_64_isr9
-    dq x86_64_isr10
-    dq x86_64_isr11
-    dq x86_64_isr12
-    dq x86_64_isr13
-    dq x86_64_isr14
-    dq x86_64_isr15
-    dq x86_64_isr16
-    dq x86_64_isr17
-    dq x86_64_isr18
-    dq x86_64_isr19
-    dq x86_64_isr20
-    dq x86_64_isr21
-    dq x86_64_isr22
-    dq x86_64_isr23
-    dq x86_64_isr24
-    dq x86_64_isr25
-    dq x86_64_isr26
-    dq x86_64_isr27
-    dq x86_64_isr28
-    dq x86_64_isr29
-    dq x86_64_isr30
-    dq x86_64_isr31
+x86_64_isr_stub_table:
+%assign i 0
+%rep 32
+    dq x86_64_isr_stub_%+i
+%assign i i+1
+%endrep
 
 align 8
-x86_64_irq_table:
-    dq x86_64_irq0
-    dq x86_64_irq1
-    dq x86_64_irq2
-    dq x86_64_irq3
-    dq x86_64_irq4
-    dq x86_64_irq5
-    dq x86_64_irq6
-    dq x86_64_irq7
-    dq x86_64_irq8
-    dq x86_64_irq9
-    dq x86_64_irq10
-    dq x86_64_irq11
-    dq x86_64_irq12
-    dq x86_64_irq13
-    dq x86_64_irq14
-    dq x86_64_irq15
+x86_64_irq_stub_table:
+%assign i 0
+%rep 16
+    dq x86_64_irq_stub_%+i
+%assign i i+1
+%endrep
