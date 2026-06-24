b8d3a51dd21fb06545334eed1078752603b84845
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-24 14:24:49 +0200

    Add x86_64 user mode kernel return trampoline

--- core/kernel/arch/x86_64/user_mode.asm
+++ core/kernel/arch/x86_64/user_mode.asm
@@ -1,4 +1,5 @@
 global x86_64_user_smoke_enter
+global x86_64_user_mode_exit_to_kernel
 extern x86_64_user_smoke_kernel_rsp
 
 section .text
@@ -13,3 +14,7 @@ x86_64_user_smoke_enter:
     push qword 0x33
     push rdi
     iretq
+
+x86_64_user_mode_exit_to_kernel:
+    mov rsp, [rel x86_64_user_smoke_kernel_rsp]
+    ret
