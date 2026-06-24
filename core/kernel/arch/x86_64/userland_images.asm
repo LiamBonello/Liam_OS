5671e5260701e46fe9193f29ebbc81245b363092
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-24 13:51:30 +0200

    Embed x86_64 hello user image

--- core/kernel/arch/x86_64/userland_images.asm
+++ core/kernel/arch/x86_64/userland_images.asm
@@ -1,8 +1,15 @@
 global x86_64_user_init_image_start
 global x86_64_user_init_image_end
+global x86_64_user_hello_image_start
+global x86_64_user_hello_image_end
 
 section .rodata
 align 16
 x86_64_user_init_image_start:
     incbin "build/x86_64/userland/init.elf"
 x86_64_user_init_image_end:
+
+align 16
+x86_64_user_hello_image_start:
+    incbin "build/x86_64/userland/hello.elf"
+x86_64_user_hello_image_end:
