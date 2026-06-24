b8392917af65fb9ef2638f6fd22266c8a2e3fbbf
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-24 14:28:47 +0200

    Embed x86_64 sysinfo user image

--- core/kernel/arch/x86_64/userland_images.asm
+++ core/kernel/arch/x86_64/userland_images.asm
@@ -2,6 +2,8 @@ global x86_64_user_init_image_start
 global x86_64_user_init_image_end
 global x86_64_user_hello_image_start
 global x86_64_user_hello_image_end
+global x86_64_user_sysinfo_image_start
+global x86_64_user_sysinfo_image_end
 
 section .rodata
 align 16
@@ -13,3 +15,8 @@ align 16
 x86_64_user_hello_image_start:
     incbin "build/x86_64/userland/hello.elf"
 x86_64_user_hello_image_end:
+
+align 16
+x86_64_user_sysinfo_image_start:
+    incbin "build/x86_64/userland/sysinfo.elf"
+x86_64_user_sysinfo_image_end:
