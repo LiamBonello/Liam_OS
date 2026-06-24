4ba7256b15325fbb653085506d17e92455d8199f
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-24 12:26:55 +0200

    Expand x86_64 VFS node metadata

--- core/kernel/arch/x86_64/vfs.h
+++ core/kernel/arch/x86_64/vfs.h
@@ -11,12 +11,15 @@
 #define X86_64_VFS_FD_BASE 3ULL
 #define X86_64_VFS_MAX_OPEN_FILES 4U
 #define X86_64_VFS_PATH_MAX_BYTES 64ULL
-#define X86_64_VFS_NO_FILE 0xFFFFFFFFU
+#define X86_64_VFS_NO_NODE 0xFFFFFFFFU
+#define X86_64_VFS_NO_FILE X86_64_VFS_NO_NODE
+#define X86_64_VFS_NODE_FILE 1U
+#define X86_64_VFS_NODE_DIRECTORY 2U
 
 struct x86_64_vfs_state {
     u32 initialized;
     u32 fd_used[X86_64_VFS_MAX_OPEN_FILES];
-    u32 fd_file_index[X86_64_VFS_MAX_OPEN_FILES];
+    u32 fd_node_index[X86_64_VFS_MAX_OPEN_FILES];
     u64 fd_offset[X86_64_VFS_MAX_OPEN_FILES];
 };
 
@@ -28,5 +31,6 @@ u64 x86_64_vfs_open(struct x86_64_vfs_state *state, const char *path, u64 flags)
 u64 x86_64_vfs_read(struct x86_64_vfs_state *state, u64 fd, char *buffer, u64 size);
 u64 x86_64_vfs_close(struct x86_64_vfs_state *state, u64 fd);
 u64 x86_64_vfs_stat(const struct x86_64_vfs_state *state, const char *path, u64 *size_out);
+u64 x86_64_vfs_type(const struct x86_64_vfs_state *state, const char *path, u64 *type_out);
 
 #endif
