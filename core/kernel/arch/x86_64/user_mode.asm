a82f4e8b8deb5f23e15bddce2c2e96a544299b56
LiamBonello <114981777+LiamBonello@users.noreply.github.com>
2026-06-25 10:18:28 +0200

    Track kernel CR3 across exec user switches

--- core/kernel/arch/x86_64/user_mode.asm
+++ core/kernel/arch/x86_64/user_mode.asm
@@ -2,6 +2,7 @@ global x86_64_user_mode_enter_init
 global x86_64_user_smoke_enter
 global x86_64_user_mode_exit_to_kernel
 extern x86_64_user_mode_kernel_rsp
+extern x86_64_user_mode_kernel_cr3
 
 section .text
 bits 64
@@ -18,5 +19,12 @@ x86_64_user_smoke_enter:
     iretq
 
 x86_64_user_mode_exit_to_kernel:
+    mov rax, [rel x86_64_user_mode_kernel_cr3]
+    test rax, rax
+    jz .restore_kernel_stack
+    mov cr3, rax
+    mov qword [rel x86_64_user_mode_kernel_cr3], 0
+
+.restore_kernel_stack:
     mov rsp, [rel x86_64_user_mode_kernel_rsp]
     ret
